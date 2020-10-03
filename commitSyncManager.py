880f67392ef041e961f8b8c0a3359f2404f66a62
Thiago Mota <thiago_mota2017@hotmail.com>
2020-10-03 11:06:39 -0300

    test

--- commitSyncManager.py
+++ commitSyncManager.py
@@ -1,15 +1,44 @@
 import subprocess
+def errorMessage(operation):
+	print(f"Something went wrong in {operation}")
 
-args = ['a.exe']
-p = subprocess.Popen(args,
+def generateMessage(operation):
+	return [f"Trying to {operation} files", f"{operation} done"]
+
+def doStagingArea():
+	try: 
+		message = generateMessage("Add")
+		print(message[0])
+		createSubProcess(['git', 'add', '.'])
+		print(message[1])
+	except: 	
+		errorMessage("Add")
+
+
+def doCommit():
+	try: 
+		message = generateMessage("Commit")
+		commitMessage = input("Please type the description for commit")
+		print(message[0])
+		createSubProcess(['git', 'commit', '-m', commitMessage])
+		print(message[1])
+	except: 	
+		errorMessage("Commit")
+
+
+def createSubProcess(args):
+	args = args
+	p = subprocess.Popen(args,
 		stdin=subprocess.PIPE,
 		stdout=subprocess.PIPE,
 		stderr=subprocess.PIPE,
 		text=True)
+	p.communicate()
 
-saida = p.communicate("Mensagem de Commit") #tentar receber por args via terminal
+doStagingArea()
+doCommit()
 
-print("\n-----------------------\nSaida: \n" + saida[0] + "\n------------------------\n");
-print("\n-----------------------\nErros: \n" + saida[1] + "\n------------------------\n");
+# print("\n-----------------------\nSaida: \n" + saida[0] + "\n------------------------\n");
+# print("\n-----------------------\nErros: \n" + saida[1] + "\n------------------------\n");
 
-print("Programa python finalizado")
+print("\n\nPrograma python finalizado")
